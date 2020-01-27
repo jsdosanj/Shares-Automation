68323fb28d2f1718354197287007381f1fc14cb7
Jasvant Singh Dosanjh <jasvantdosanjh@outlook.com>
2020-01-27 14:51:23 -0500

    Updated sharesautomation.ps1
    Added more robust input for users, and added email forwarding for approval.

--- SharesAutomation.ps1
+++ SharesAutomation.ps1
@@ -4,14 +4,19 @@
   <Overview of script>
 .DESCRIPTION
   <Brief description of script>
+  <This script will allow users to enter their info, and the script will search the shares and 
+  AD to find the user
+  and give them access to the shares upon approval, and give them the access/ownership.>
 .PARAMETER <Parameter_Name>
     <Brief description of parameter input required. Repeat this attribute if required>
 .INPUTS
   <Inputs if any, otherwise state None>
-  <This is where the user would enter their name, university email, and what shares they would like access to>
+  <This is where the user would enter their name, university email, 
+  and what shares they would like access to>
 .OUTPUTS
   <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
-  <This is where the the script would read the inputs and verify that the user is allowed access to the drive they are requesting.>
+  <This is where the the script would read the inputs 
+  and verify that the user is allowed access to the drive they are requesting.>
     <From there, the script would notify the correct owner(s) of the share for approval>
 .NOTES
   Version:        1.0
@@ -23,22 +28,25 @@
   <Example goes here. Repeat this attribute for more than one example>
     Input 1: Ask for user input
   #>
-  $Share = Read-Host -Prompt 'Input the Share name you are requesting'
+  $Share = Read-Host -Prompt 'What Share(S) Do You Need Access To'
+    if ($Share) {
+	    Write-Host "You have requested access to the [$Share] share."
+    } else {
+	    Write-Warning -Message "Please enter the correct Shares name."
+    }
   $UserName = Read-Host -Prompt 'Input your Full Name'
-  $Uni_Email = Read-Host =-Prompt 'Input your Oakland University Email'
-  $Date = Get-Date
-  Write-Host "You input server '$Share' and '$Username'  and '$Uni_Email' on '$Date'" 
-  
+  $Uni_Email = Read-Host =-Prompt 'Input your Oakland University Email'  
 
-#---------------------------------------------------------[Initialisations]--------------------------------------------------------
+#---------------------------------------------------------[Initialisations]-------------------------
 
 #Set Error Action to Silently Continue
 $ErrorActionPreference = "SilentlyContinue"
 
 #Dot Source required Function Libraries
 . "C:\Scripts\Functions\Logging_Functions.ps1"
-
-#----------------------------------------------------------[Declarations]----------------------------------------------------------
+$Date = Get-Date
+  Write-Host "You input server '$Share' and '$Username'  and '$Uni_Email' on '$Date'"
+#----------------------------------------------------------[Declarations]--------------------------
 
 #Script Version
 $sScriptVersion = "1.0"
@@ -47,8 +55,11 @@ $sScriptVersion = "1.0"
 $sLogPath = "C:\Windows\Temp"
 $sLogName = "<script_name>.log"
 $sLogFile = Join-Path -Path $sLogPath -ChildPath $sLogName
+Invoke-Expression "$Script:psouservertools:getoushare.ps1 $argumentList"
 
-#-----------------------------------------------------------[Functions]------------------------------------------------------------
+Invoke-Item (start powershell ((Split-Path $MyInvocation.InvocationName) + "\send-ouServerNetTicket.ps1"))
+
+#-----------------------------------------------------------[Functions]---------------------------
 
 <#
 Function <FunctionName>{
@@ -77,27 +88,8 @@ Function <FunctionName>{
   }
 }
 #>
-$UserName = Read-Host -Prompt 'Please enter your first and last name'
-if ($UserName) {
-    Write-Host "You are: [$UserName]"
-}
-function findUser ([string]$UserName)
-{
- Get-ADUser -filter "name -like '$UserName*'" -properties ipphone | ft Name, ipphone
-}
 
-$Share = Read-Host -Prompt 'Server name to process'
-if ($Share) {
-	Write-Host "You have requested access to the [$Share] share."
-} else {
-	Write-Warning -Message "Please enter the correct Shares name."
-}
-function findShare ([string]$Share)
-{
- Get-ADUser -filter "name -like '$Share*'" -properties ipphone | ft Name, ipphone
-}
-
-#-----------------------------------------------------------[Execution]------------------------------------------------------------
+#-----------------------------------------------------------[Execution]-----------------------
 
 #Log-Start -LogPath $sLogPath -LogName $sLogName -ScriptVersion $sScriptVersion
 #Script Execution goes here
